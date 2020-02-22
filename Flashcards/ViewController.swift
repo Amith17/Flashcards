fbd1126402b786f4238301b32808fe70722bcafa
Amith Rana Revelly <revelly.amithrana@gmail.com>
2020-02-21 21:25:51 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Amith Rana on 2/21/20.
+//  Copyright © 2020 Amith Rana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
